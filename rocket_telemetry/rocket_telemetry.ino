ca8637e57d18286506e88d4511fd88d686245a13
AvidCoder27 <gilireeves@gmail.com>
2025-04-08 20:04:18 -0400

    read to test on arduino

--- rocket_telemetry/rocket_telemetry.ino
+++ rocket_telemetry/rocket_telemetry.ino
@@ -1,92 +1,153 @@
+#include <Adafruit_BMP280.h>
 #include <Adafruit_ICM20649.h>
-#include <Adafruit_ICM20948.h>
-#include <Adafruit_ICM20X.h>
-#include <SPI.h>
 #include <SD.h>
-#include <Wire.h>
 
 /*
-  SD card datalogger
   SD card attached to SPI bus as follows:
- ** SDO - pin 11
- ** SDI - pin 12
- ** CLK - pin 13
- ** CS - pin 10
+  ** SDO - pin 11
+  ** SDI - pin 12
+  ** CLK - pin 13 (SCK)
+  ** CS  - pin 10
+
+  IMU pinout: (https://learn.adafruit.com/adafruit-icm20649-wide-range-6-dof-imu-accelerometer-and-gyro/pinouts)
+  default i2c address at 0x68 (adafruit lib handles it)
+  ** SCL - i2c clock pin (has pull-up)
+  ** SDA - i2c data pin (has pull-up)
+  ** AD0 - pull high to switch address to 0x69
+
+  BMP pinout: (https://learn.adafruit.com/adafruit-bmp280-barometric-pressure-plus-temperature-sensor-breakout/pinouts)
+  default i2c address at 0x77 (adafruit lib handles it)
+  ** SCK/SCL - i2c clock pin, id 
+  ** SDI/SDA - i2c data pin
+
+  SPI Clock is pin 13
+
+  I2C clock is pin A5 (D19).
+  Both i2c devices connect to that on their clock pin.
+  I2C data is SDA on pin A4 (D18).
+  Both i2c devices connect to that on their data pin.
 */
 
-const int sdChipSelect = 10;
+const int SD_CHIP_SELECT = 10;
 
-Adafruit_ICM20649 icm; // imu
-Adafruit_Sensor *icm_accel, *icm_gyro;
+Adafruit_ICM20649 imu; // imu
+Adafruit_Sensor *imu_accel, *imu_gyro;
+
+Adafruit_BMP280 bmp; // barometric pressure
+Adafruit_Sensor *bmp_pressure;
+
+File log_file;
+
+inline void waitForever() {
+  while(1) {
+    delay(10);
+  }
+}
+
+inline void sd_init() {
+  Serial.print("Initializing SD card... ");
+  if (!SD.begin(SD_CHIP_SELECT)) {
+    Serial.println("SD init failed");
+    // Things to check:
+    // 1. is a card inserted?
+    // 2. is your wiring correct?
+    // 3. did you change the chipSelect pin to match your shield or module?
+    // Note: press reset button on the board and reopen this Serial Monitor after fixing your issue!
+    waitForever();
+  }
+  Serial.println("SD success!");
+}
+
+inline void imu_init() {
+  Serial.print("Initializing IMU... ");
+
+  // open i2c comms with imu
+  if (!imu.begin_I2C()) {
+    Serial.println("Failed to find IMU");
+    waitForever();
+  }
+
+  imu_accel = imu.getAccelerometerSensor();
+  imu_accel->printSensorDetails();
+
+  imu_gyro = imu.getGyroSensor();
+  imu_gyro->printSensorDetails();
+
+  Serial.println("IMU success!");
+}
+
+inline void bmp_init() {
+  Serial.print("Initializing BMP... ");
+  if (!bmp.begin()) {
+    Serial.print("Failed to find BMP, SensorID was: 0x");
+    Serial.println(bmp.sensorID(),16);
+    waitForever();
+    // ID of 0xFF probably means a bad address, a BMP 180 or BMP 085
+    // ID of 0x56-0x58 represents a BMP 280
+    // ID of 0x60 represents a BME 280
+    // ID of 0x61 represents a BME 680
+  }
+
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+  bmp_pressure = bmp.getPressureSensor();
+  bmp_pressure->printSensorDetails();
+
+  Serial.println("BMP success!");
+}
+
+void open_log_file() {
+  Serial.print("Opening log file... ");
+  log_file = SD.open("datalog.txt", FILE_WRITE);
+  if (!log_file) {
+    Serial.println("Failed to open datalog.txt");
+  }
+  Serial.println("Log file opened!");
+}
 
 void setup() {
   // Open serial communications
   Serial.begin(9600);
   // wait for Serial Monitor
   while (!Serial);
-  
-  // open i2c comms with imu
-  if (!icm.begin_I2C()) {
-    Serial.println("Failed to find ICM20649 chip");
-    while (true) {
-      delay(10);
-    }
-  }
 
-  Serial.println("ICM20649 found");
-  
-  icm_accel = icm.getAccelerometerSensor();
-  icm_accel->printSensorDetails();
+  sd_init();
+  open_log_file();
+  imu_init();
+  bmp_init();
 
-  icm_gyro = icm.getGyroSensor();
-  icm_gyro->printSensorDetails();
-
-  Serial.print("Initializing SD card...");
-
-  if (!SD.begin(sdChipSelect)) {
-    Serial.println("initialization failed.");
-    // Things to check:
-    // 1. is a card inserted?
-    // 2. is your wiring correct?
-    // 3. did you change the chipSelect pin to match your shield or module?
-    // Note: press reset button on the board and reopen this Serial Monitor after fixing your issue!
-    while (true) {
-      delay(10);
-    }
-  }
-
-  Serial.println("initialization complete");
+  Serial.println("Initialization complete!");
 }
 
 void loop() {
+  // get sensor data
+  sensors_event_t accel_evt, gyro_evt, pressure_evt;
+  bmp_pressure->getEvent(&pressure_evt);
+  imu_accel->getEvent(&accel_evt);
+  imu_gyro->getEvent(&gyro_evt);
+  
   // make a string for assembling the data to log
-  // [time ms],[accel x],[accel y],[accel z],[gyro x],[gyro y],[gyro z]
-  String dataString = String(millis());
+  // [time],[accel x],[accel y],[accel z],[gyro x],[gyro y],[gyro z],[pressure]
 
-  // get accel data
-  sensors_event_t accel;
-  sensors_event_t gyro;
-  icm_accel->getEvent(&accel);
-  icm_gyro->getEvent(&gyro);
+  // time in milliseconds
   // accel in m/s^2
-  dataString += ',' + accel.acceleration.x + ',' + accel.acceleration.y + ',' + accel.acceleration.z;
   // gyro in rad/sec
-  dataString += ',' + gyro.gyro.x + ',' + gyro.gyro.y + ',' + gyro.gyro.z; 
+  // pressure in hPa
+  String dataString = "";
+  dataString += millis() + ',' + accel_evt.acceleration.x + ',' + accel_evt.acceleration.y + ',' + accel_evt.acceleration.z + ',' + gyro_evt.gyro.x + ',' + gyro_evt.gyro.y + ',' + gyro_evt.gyro.z + ',' + pressure_evt.pressure;
 
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
-
-  // if the file is available, write to it:
-  if (dataFile) {
-    dataFile.println(dataString);
-    dataFile.close();
+  // if the file is available, write to it
+  if (log_file) {
+    log_file.println(dataString);    
     // print to the serial port too:
-    Serial.print("DataString: ");
     Serial.println(dataString);
   }
-  // if the file isn't open, pop up an error:
+  // if the file isn't open, pop up an error
   else {
-    Serial.println("!!!!error opening datalog.txt!!!!");
+    Serial.println("ERROR opening datalog.txt !!!!");
   }
 }

dd88720c292edefd6dc09b6bb43b7d40e01a12f1
AvidCoder27 <gilireeves@gmail.com>
2025-04-12 00:51:42 -0400

    no pressure sensor ig

--- rocket_telemetry/rocket_telemetry.ino
+++ rocket_telemetry/rocket_telemetry.ino
@@ -31,9 +31,6 @@
 
 const int SD_CHIP_SELECT = 10;
 
-Adafruit_BMP280 bmp; // barometric pressure
-Adafruit_Sensor *bmp_pressure;
-
 Adafruit_ICM20649 imu; // imu
 Adafruit_Sensor *imu_accel, *imu_gyro;
 
@@ -82,28 +79,6 @@ void init_imu() {
   Serial.println("IMU success!");
 }
 
-void init_bmp() {
-  Serial.print("Initializing BMP... ");
-  if (!bmp.begin()) {
-    Serial.print("Failed to find BMP, SensorID was: 0x");
-    Serial.println(bmp.sensorID(),16);
-    waitForever();
-    // ID of 0xFF probably means a bad address, a BMP 180 or BMP 085
-    // ID of 0x56-0x58 represents a BMP 280
-    // ID of 0x60 represents a BME 280
-    // ID of 0x61 represents a BME 680
-  }
-
-  // bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-  //               Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-  //               Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-  //               Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-  //               Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-
-  bmp_pressure = bmp.getPressureSensor();
-  Serial.println("BMP success!");
-}
-
 void setup() {
   // Open serial communications
   Serial.begin(9600);
@@ -111,17 +86,7 @@ void setup() {
   while (!Serial) delay(10);
   Serial.println("Started serial communications");
 
-  Wire.end();
-  delay(10);
-  Wire.begin();
-  delay(10);
-
-  delay(100);
-  // init_bmp();
-  delay(100);
   init_imu();
-  delay(100);
-
   sd_init();
   open_log_file();
 
@@ -129,15 +94,13 @@ void setup() {
 }
 
 void loop() {  
-  return;
   // get sensor data
-  sensors_event_t accel_evt, gyro_evt, pressure_evt;
-  bmp_pressure->getEvent(&pressure_evt);
+  sensors_event_t accel_evt, gyro_evt;
   imu_accel->getEvent(&accel_evt);
   imu_gyro->getEvent(&gyro_evt);
   
   // make a string for assembling the data to log
-  // [time],[accel x],[accel y],[accel z],[gyro x],[gyro y],[gyro z],[altitude by pressure]
+  // [time],[accel x],[accel y],[accel z],[gyro x],[gyro y],[gyro z]
 
   // time in milliseconds
   // accel in m/s^2
@@ -157,9 +120,7 @@ void loop() {
   Serial.print(",");
   Serial.print(gyro_evt.gyro.y);
   Serial.print(",");
-  Serial.print(gyro_evt.gyro.z);
-  Serial.print(",");
-  Serial.println(pressure_evt.pressure);
+  Serial.println(gyro_evt.gyro.z);
 
   // if the file is available, write to it
   if (log_file) {
@@ -175,9 +136,7 @@ void loop() {
     log_file.print(",");
     log_file.print(gyro_evt.gyro.y);
     log_file.print(",");
-    log_file.print(gyro_evt.gyro.z);
-    log_file.print(",");
-    log_file.println(pressure_evt.pressure);
+    log_file.println(gyro_evt.gyro.z);
   }
   // if the file isn't open, pop up an error
   else {
